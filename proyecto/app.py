b6d54338dd987c5968a7dd1dd73b5c4aa70ef3bf
Anggy Marcela Arroyave <anggym.arroyaveg@uqvirtual.edu.co>
2024-11-10 14:46:18 -0500

    Se modifica front

--- proyecto/app.py
+++ proyecto/app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, render_template, url_for, request
 from flask_cors import CORS  # Permitir solicitudes desde un dominio diferente
 
 app = Flask(__name__)
@@ -13,9 +13,8 @@ def ejecutar_funcion():
 
 @app.route('/api/chart-2', methods=['POST'])
 def ejecutar_chart():
-    # Lógica de negocio para la segunda ruta
-    resultado = {"mensaje": "Función chart"}
-    return jsonify(resultado)
+    image_url = url_for('static', filename='Entrega 1\resultados\wordcloud.png')
+    return render_template('index.html', image_url=image_url)
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))  # Usa el puerto asignado por Render o el puerto 5000 por defecto
