f073071cc5da25a462fca0aff8aa364e75b36540
unknown <jupadelg@bancolombia.com.co>
2024-11-10 14:34:40 -0500

    holi

--- proyecto/Entrega 1/entrega1.py
+++ proyecto/Entrega 1/entrega1.py
@@ -10,25 +10,108 @@ from collections import Counter
 
 warnings.filterwarnings('ignore')
 
-# Crear carpeta de resultados
-if not os.path.exists('resultados'):
-    os.makedirs('resultados')
+# Crear carpeta de resultados con subcarpetas organizadas
+base_path = 'resultados'
+subcarpetas = ['Requerimiento_1', 'Requerimiento_2', 'Requerimiento_3', 'Requerimiento_4', 'Requerimiento_5']
+for subcarpeta in subcarpetas:
+    os.makedirs(os.path.join(base_path, subcarpeta), exist_ok=True)
 
 class BibliometricAnalyzer:
     def __init__(self):
         self.unified_data = None
         self.categories = {
-            'Habilidades': ['Abstraction', 'Algorithm', 'Algorithmic thinking', 'Coding', 
-                           'Collaboration', 'Cooperation', 'Creativity', 'Critical thinking',
-                           'Debug', 'Decomposition', 'Evaluation', 'Generalization', 'Logic',
-                           'Logical thinking', 'Modularity', 'Patterns recognition', 
-                           'Problem solving', 'Programming', 'Representation', 'Reuse', 'Simulation'],
-            'Conceptos Computacionales': ['Conditionals', 'Control structures', 'Directions', 
-                                        'Events', 'Functions', 'Loops', 'Modular structure',
-                                        'Parallelism', 'Sequences', 'Software', 'Hardware', 'Variables']
+            'Habilidades': [
+                'Abstraction', 'Algorithm', 'Algorithmic thinking', 'Coding',
+                'Collaboration', 'Cooperation', 'Creativity', 'Critical thinking',
+                'Debug', 'Decomposition', 'Evaluation', 'Generalization', 'Logic',
+                'Logical thinking', 'Modularity', 'Patterns recognition',
+                'Problem solving', 'Programming', 'Representation', 'Reuse', 'Simulation'
+            ],
+            'Conceptos Computacionales': [
+                'Conditionals', 'Control structures', 'Directions', 'Events',
+                'Functions', 'Loops', 'Modular structure', 'Parallelism',
+                'Sequences', 'Software', 'Hardware', 'Variables'
+            ],
+            'Actitudes': [
+                'Emotional', 'Engagement', 'Motivation', 'Perceptions',
+                'Persistence', 'Self-efficacy', 'Self-perceived'
+            ],
+            'Propiedades Psicométricas': [
+                'Classical Test Theory - CTT', 'Confirmatory Factor Analysis - CFA',
+                'Exploratory Factor Analysis - EFA', 'Item Response Theory - IRT',
+                'Reliability', 'Structural Equation Model - SEM', 'Validity'
+            ],
+            'Herramienta de Evaluación': [
+                'Beginners Computational Thinking test - BCTt', 'Coding Attitudes Survey - ESCAS',
+                'Collaborative Computing Observation Instrument', 'Competent Computational Thinking test - cCTt',
+                'Computational thinking skills test - CTST', 'Computational concepts',
+                'Computational Thinking Assessment for Chinese Elementary Students - CTA-CES',
+                'Computational Thinking Challenge - CTC', 'Computational Thinking Levels Scale - CTLS',
+                'Computational Thinking Scale - CTS', 'Computational Thinking Test - CTt',
+                'Computational Thinking Test for Elementary School Students - CTT-ES',
+                'Computational Thinking Test for Lower Primary - CTtLP',
+                'Computational thinking-skill tasks on numbers and arithmetic',
+                'Computerized Adaptive Programming Concepts Test - CAPCT',
+                'CT Scale - CTS', 'Elementary Student Coding Attitudes Survey - ESCAS',
+                'General self-efficacy scale', 'ICT competency test',
+                'Instrument of computational identity', 'KBIT fluid intelligence subtest',
+                'Mastery of computational concepts Test and an Algorithmic Test',
+                'Multidimensional 21st Century Skills Scale', 'Self-efficacy scale',
+                'STEM learning attitude scale - STEM-LAS', 'The computational thinking scale'
+            ],
+            'Diseño de Investigación': [
+                'No experimental', 'Experimental', 'Longitudinal research',
+                'Mixed methods', 'Post-test', 'Pre-test', 'Quasi-experiments'
+            ],
+            'Nivel de Escolaridad': [
+                'Upper elementary education - Upper elementary school',
+                'Primary school - Primary education - Elementary school',
+                'Early childhood education – Kindergarten - Preschool',
+                'Secondary school - Secondary education', 'High school - Higher education',
+                'University – College'
+            ],
+            'Medio': [
+                'Block programming', 'Mobile application', 'Pair programming',
+                'Plugged activities', 'Programming', 'Robotics',
+                'Spreadsheet', 'STEM', 'Unplugged activities'
+            ],
+            'Estrategia': [
+                'Construct-by-self mind mapping - CBS-MM', 'Construct-on-scaffold mind mapping - COS-MM',
+                'Design-based learning - CTDBL', 'Design-based learning - DBL',
+                'Evidence-centred design approach', 'Gamification',
+                'Reverse engineering pedagogy - REP', 'Technology-enhanced learning',
+                'Collaborative learning', 'Cooperative learning', 'Flipped classroom',
+                'Game-based learning', 'Inquiry-based learning', 'Personalized learning',
+                'Problem-based learning', 'Project-based learning', 'Universal design for learning'
+            ],
+            'Herramienta': [
+                'Alice', 'Arduino', 'Scratch', 'ScratchJr', 'Blockly Games', 'Code.org',
+                'Codecombat', 'CSUnplugged', 'Robot Turtles', 'Hello Ruby', 'Kodable',
+                'LightbotJr', 'KIBO robots', 'BEE BOT', 'CUBETTO', 'Minecraft',
+                'Agent Sheets', 'Mimo', 'Py– Learn', 'SpaceChem'
+            ]
         }
 
-    # Requerimiento 1: Procesamiento de cada fuente de datos y normalización
+    # Requerimiento 1: Unificar datos y eliminar duplicados
+    def load_and_unify_data(self):
+        try:
+            ieee1 = pd.read_csv('export_sanitized.csv')
+            ieee2 = pd.read_csv('export2024.10.15-18.47.42.csv')
+            oxford = pd.read_csv('Oxford Academic Title List Export 03 November 2024.csv')
+            scopus = pd.read_csv('scopus_cleaned.csv')
+            acm = pd.read_csv('unified_acm_data.csv')
+
+            dfs = [self.process_ieee_export(ieee1), self.process_ieee_export(ieee2),
+                   self.process_oxford(oxford), self.process_scopus(scopus), self.process_acm(acm)]
+            self.unified_data = pd.concat(dfs, ignore_index=True)
+            self.unified_data['DOI'] = self.unified_data['DOI'].fillna('')
+            self.unified_data = self.unified_data.drop_duplicates(subset=['Title', 'DOI', 'Publisher'], keep='first')
+            self.unified_data.to_csv(os.path.join(base_path, 'Requerimiento_1', 'unified_database.csv'), index=False)
+            print("Unificación completada. Archivo guardado en 'resultados/Requerimiento_1/unified_database.csv'")
+        except Exception as e:
+            print(f"Error al unificar los datos: {e}")
+
+    # Funciones de procesamiento de cada fuente de datos
     def process_ieee_export(self, df):
         processed_df = pd.DataFrame()
         processed_df['Title'] = df['Document Title']
@@ -81,26 +164,7 @@ class BibliometricAnalyzer:
         processed_df['Keywords'] = ''
         return processed_df
 
-    # Requerimiento 1: Unificación de datos y eliminación de duplicados
-    def load_and_unify_data(self):
-        try:
-            ieee1 = pd.read_csv('export_sanitized.csv')
-            ieee2 = pd.read_csv('export2024.10.15-18.47.42.csv')
-            oxford = pd.read_csv('Oxford Academic Title List Export 03 November 2024.csv')
-            scopus = pd.read_csv('scopus_cleaned.csv')
-            acm = pd.read_csv('unified_acm_data.csv')
-
-            dfs = [self.process_ieee_export(ieee1), self.process_ieee_export(ieee2),
-                   self.process_oxford(oxford), self.process_scopus(scopus), self.process_acm(acm)]
-            self.unified_data = pd.concat(dfs, ignore_index=True)
-            self.unified_data['DOI'] = self.unified_data['DOI'].fillna('')
-            self.unified_data = self.unified_data.drop_duplicates(subset=['Title', 'DOI', 'Publisher'], keep='first')
-            self.unified_data.to_csv('resultados/unified_database.csv', index=False)
-            print("Unificación completada. Archivo guardado en 'resultados/unified_database.csv'")
-        except Exception as e:
-            print(f"Error al unificar los datos: {e}")
-
-    # Requerimiento 2: Generación de estadísticas descriptivas
+    # Requerimiento 2: Generar estadísticas descriptivas
     def generate_descriptive_stats(self):
         if self.unified_data is None:
             return None
@@ -111,34 +175,40 @@ class BibliometricAnalyzer:
             'publications_by_source': self.unified_data.groupby('source').size().to_dict(),
             'publications_by_publisher': self.unified_data.groupby('Publisher').size().nlargest(10).to_dict()
         }
-        pd.DataFrame(stats['top_authors'].items(), columns=['Author', 'Count']).to_csv('resultados/top_authors.csv', index=False)
-        pd.DataFrame(stats['publications_by_year'].items(), columns=['Year', 'Count']).to_csv('resultados/publications_by_year.csv', index=False)
-        pd.DataFrame(stats['top_cited_articles']).to_csv('resultados/top_cited_articles.csv', index=False)
-        pd.DataFrame(stats['publications_by_source'].items(), columns=['Source', 'Count']).to_csv('resultados/publications_by_source.csv', index=False)
-        pd.DataFrame(stats['publications_by_publisher'].items(), columns=['Publisher', 'Count']).to_csv('resultados/publications_by_publisher.csv', index=False)
-        print("Estadísticas descriptivas generadas y guardadas en 'resultados'.")
+        pd.DataFrame(stats['top_authors'].items(), columns=['Author', 'Count']).to_csv(
+            os.path.join(base_path, 'Requerimiento_2', 'top_authors.csv'), index=False)
+        pd.DataFrame(stats['publications_by_year'].items(), columns=['Year', 'Count']).to_csv(
+            os.path.join(base_path, 'Requerimiento_2', 'publications_by_year.csv'), index=False)
+        pd.DataFrame(stats['top_cited_articles']).to_csv(
+            os.path.join(base_path, 'Requerimiento_2', 'top_cited_articles.csv'), index=False)
+        pd.DataFrame(stats['publications_by_source'].items(), columns=['Source', 'Count']).to_csv(
+            os.path.join(base_path, 'Requerimiento_2', 'publications_by_source.csv'), index=False)
+        pd.DataFrame(stats['publications_by_publisher'].items(), columns=['Publisher', 'Count']).to_csv(
+            os.path.join(base_path, 'Requerimiento_2', 'publications_by_publisher.csv'), index=False)
+        print("Estadísticas descriptivas generadas y guardadas en 'resultados/Requerimiento_2'.")
 
-    # Requerimiento 3: Análisis de categorías específicas en abstracts
+    # Requerimiento 3: Análisis de categorías en abstracts
     def analyze_categories_in_abstracts(self):
         abstract_text = ' '.join(self.unified_data['Abstract'].dropna())
         category_counts = {}
         for category, keywords in self.categories.items():
             category_counts[category] = sum(keyword.lower() in abstract_text.lower() for keyword in keywords)
-        pd.DataFrame(category_counts.items(), columns=['Category', 'Count']).to_csv('resultados/category_counts.csv', index=False)
-        print("Análisis de categorías completado y guardado en 'resultados/category_counts.csv'.")
+        pd.DataFrame(category_counts.items(), columns=['Category', 'Count']).to_csv(
+            os.path.join(base_path, 'Requerimiento_3', 'category_counts.csv'), index=False)
+        print("Análisis de categorías completado y guardado en 'resultados/Requerimiento_3/category_counts.csv'.")
 
-    # Requerimiento 4: Generación de nube de palabras
+    # Requerimiento 4: Generar nube de palabras
     def generate_word_cloud(self):
         text = ' '.join(self.unified_data['Abstract'].fillna('') + ' ' + self.unified_data['Keywords'].fillna(''))
         wordcloud = WordCloud(width=1200, height=800, background_color='white').generate(text)
         plt.figure(figsize=(10, 8))
         plt.imshow(wordcloud, interpolation='bilinear')
         plt.axis('off')
-        plt.savefig('resultados/wordcloud.png', bbox_inches='tight')
+        plt.savefig(os.path.join(base_path, 'Requerimiento_4', 'wordcloud.png'), bbox_inches='tight')
         plt.close()
-        print("Nube de palabras generada y guardada en 'resultados/wordcloud.png'.")
+        print("Nube de palabras generada y guardada en 'resultados/Requerimiento_4/wordcloud.png'.")
 
-    # Requerimiento 5: Creación de grafo para los journals y artículos más citados
+    # Requerimiento 5: Crear grafo de journals y artículos más citados
     def generate_journal_graph(self):
         top_journals = self.unified_data['Publisher'].value_counts().nlargest(10).index
         journal_data = self.unified_data[self.unified_data['Publisher'].isin(top_journals)]
@@ -160,74 +230,9 @@ class BibliometricAnalyzer:
         nx.draw_networkx_labels(G, pos, font_size=8)
         plt.title("Relación entre Journals y Artículos Más Citados")
         plt.legend(['Journals', 'Articles'])
-        plt.savefig('resultados/journal_graph.png', bbox_inches='tight')
+        plt.savefig(os.path.join(base_path, 'Requerimiento_5', 'journal_graph.png'), bbox_inches='tight')
         plt.close()
-        print("Grafo de journals y artículos más citados generado y guardado en 'resultados/journal_graph.png'.")
-
-        # CARGAR PNGS ESTADISTICAS
-
-        # Cargar los archivos CSV generados
-        top_authors_df = pd.read_csv('resultados/top_authors.csv')
-        publications_by_year_df = pd.read_csv('resultados/publications_by_year.csv')
-        top_cited_articles_df = pd.read_csv('resultados/top_cited_articles.csv')
-        publications_by_source_df = pd.read_csv('resultados/publications_by_source.csv')
-        publications_by_publisher_df = pd.read_csv('resultados/publications_by_publisher.csv')
-
-
-    # Gráfico 1: Top autores
-        def plot_top_authors():
-            top_authors_df.plot(kind='bar', x='Author', y='Count', legend=False, color='skyblue')
-            plt.title('Top 15 Autores')
-            plt.xlabel('Autor')
-            plt.ylabel('Cantidad de Publicaciones')
-            plt.xticks(rotation=45, ha='right')
-            plt.tight_layout()
-            plt.savefig('resultados/top_authors.png')  # Guardar como PNG
-            plt.close()  # Cerrar la figura para liberar recursos
-
-        # Gráfico 2: Publicaciones por año
-        def plot_publications_by_year():
-            publications_by_year_df.plot(kind='line', x='Year', y='Count', marker='o', color='green')
-            plt.title('Publicaciones por Año')
-            plt.xlabel('Año')
-            plt.ylabel('Cantidad de Publicaciones')
-            plt.xticks(rotation=45)
-            plt.tight_layout()
-            plt.savefig('resultados/publications_by_year.png')  # Guardar como PNG
-            plt.close()
-
-        # Gráfico 3: Top artículos más citados
-        def plot_top_cited_articles():
-            top_cited_articles_df.plot(kind='bar', x='Title', y='citations', legend=False, color='orange')
-            plt.title('Top 15 Artículos Más Citados')
-            plt.xlabel('Título del Artículo')
-            plt.ylabel('Número de Citaciones')
-            plt.xticks(rotation=90, ha='center')
-            plt.tight_layout()
-            plt.savefig('resultados/top_cited_articles.png')  # Guardar como PNG
-            plt.close()
-
-        # Gráfico 4: Publicaciones por fuente
-        def plot_publications_by_source():
-            publications_by_source_df.plot(kind='bar', x='Source', y='Count', legend=False, color='purple')
-            plt.title('Publicaciones por Fuente')
-            plt.xlabel('Fuente')
-            plt.ylabel('Cantidad de Publicaciones')
-            plt.xticks(rotation=45, ha='right')
-            plt.tight_layout()
-            plt.savefig('resultados/publications_by_source.png')  # Guardar como PNG
-            plt.close()
-
-        # Gráfico 5: Publicaciones por editor
-        def plot_publications_by_publisher():
-            publications_by_publisher_df.plot(kind='bar', x='Publisher', y='Count', legend=False, color='lightcoral')
-            plt.title('Publicaciones por Editor')
-            plt.xlabel('Editor')
-            plt.ylabel('Cantidad de Publicaciones')
-            plt.xticks(rotation=45, ha='right')
-            plt.tight_layout()
-            plt.savefig('resultados/publications_by_publisher.png')  # Guardar como PNG
-            plt.close()
+        print("Grafo de journals y artículos más citados generado y guardado en 'resultados/Requerimiento_5/journal_graph.png'.")
 
 # Función principal para ejecutar los requerimientos del proyecto
 def main():
@@ -250,27 +255,10 @@ def main():
         # Requerimiento 5: Grafo de journals y artículos más citados
         analyzer.generate_journal_graph()
 
-        # Gráfico 1: Top autores
-        analyzer.plot_top_authors()
-
-        # Gráfico 2: Publicaciones por año
-        analyzer.plot_publications_by_year()
-
-        # Gráfico 3: Top artículos más citados
-        analyzer.plot_top_cited_articles()
-
-        # Gráfico 4: Publicaciones por fuente
-        analyzer.plot_publications_by_source()
-
-        # Gráfico 5: Publicaciones por editor
-        analyzer.plot_publications_by_publisher()
-
-       
-        print("\nAnálisis completado exitosamente. Todos los resultados están en la carpeta 'resultados'.")
+        print("\nAnálisis completado exitosamente. Todos los resultados están organizados en la carpeta 'resultados'.")
         
     except Exception as e:
         print(f"Error durante la ejecución: {str(e)}")
 
 if __name__ == "__main__":
     main()
-
