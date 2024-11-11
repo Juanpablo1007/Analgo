c966f905ede2f5c204987f5fad11a8de308d3e32
unknown <jupadelg@bancolombia.com.co>
2024-11-11 08:33:15 -0500

    holi

--- proyecto/front/public/script.js
+++ proyecto/front/public/script.js
@@ -1,107 +1,88 @@
-// Realiza petición al back endpoint requerimientos 3
-document.getElementById('boton-cargar').addEventListener('click', async () => {
-  event.preventDefault();
-  try {
-      const respuesta = await fetch('http://127.0.0.1:5000/api/ejecutar_funcion', {
-          method: 'POST',
-          headers: { 'Content-Type': 'application/json' }
-      });
-      const data = await respuesta.json();
-      console.log(data)
-  } catch (error) {
-      console.error('Error al ejecutar función:', error);
-  }
-});
-
 document.addEventListener('DOMContentLoaded', function() {
-  // Asocia cada botón a su respectiva sección
+  // Botón para el contexto
   document.getElementById('boton-contexto').addEventListener('click', function() {
     mostrarSeccion('estadisticas');
     actualizarClaseActive('boton-contexto');
   });
 
+  
+  function descargarArchivo() {
+    // Cambia la ruta por la ubicación real en tu servidor
+    const rutaArchivo = 'resultados/Requerimiento_1/unified_database.csv';
+
+    // Crear un enlace y simular la descarga
+    const enlace = document.createElement('a');
+    enlace.href = rutaArchivo;
+    enlace.download = 'unified_database.csv'; // Nombre del archivo para descargar
+    enlace.click();
+}
+
+
+  // Botón para Requerimiento 1
   document.getElementById('boton-requerimiento-1').addEventListener('click', function() {
     mostrarSeccion('requerimiento1');
     actualizarClaseActive('boton-requerimiento-1');
   });
 
+  // Botón para Requerimiento 2
   document.getElementById('boton-requerimiento-2').addEventListener('click', async () => {
     try {
       const respuesta = await fetch('http://127.0.0.1:5000/api/requerimientos/2', {
-          method: 'GET',
-          headers: { 'Content-Type': 'application/json' }
+        method: 'GET',
+        headers: { 'Content-Type': 'application/json' }
       });
       const data = await respuesta.json();
-      document.getElementById('image_afiliaciones_counts').src = `data:image/jpeg;base64,${data.image_afiliaciones_counts}`;
-      document.getElementById('image_articulos_citados_counts').src = `data:image/jpeg;base64,${data.image_articulos_citados_counts}`;
-      document.getElementById('image_autor_pais_counts').src = `data:image/jpeg;base64,${data.image_autor_pais_counts}`;
-      document.getElementById('image_autor_publisher_counts').src = `data:image/jpeg;base64,${data.image_autor_publisher_counts}`;
-      document.getElementById('image_autores_citados_counts').src = `data:image/jpeg;base64,${data.image_autores_citados_counts}`;
-      document.getElementById('image_base_autor_counts').src = `data:image/jpeg;base64,${data.image_base_autor_counts}`;
-      document.getElementById('image_base_datos_counts').src = `data:image/jpeg;base64,${data.image_base_datos_counts}`;
-      document.getElementById('image_journal_articulo_counts').src = `data:image/jpeg;base64,${data.image_journal_articulo_counts}`;
-      document.getElementById('image_journals_counts').src = `data:image/jpeg;base64,${data.image_journals_counts}`;
-      document.getElementById('image_productos_por_tipo_counts').src = `data:image/jpeg;base64,${data.image_productos_por_tipo_counts}`;
-      document.getElementById('image_publicaciones_por_año_counts').src = `data:image/jpeg;base64,${data.image_publicaciones_por_año_counts}`;
-      document.getElementById('image_publishers_counts').src = `data:image/jpeg;base64,${data.image_publishers_counts}`;
-      document.getElementById('image_tipo_producto_año_counts').src = `data:image/jpeg;base64,${data.image_tipo_producto_año_counts}`;
-
+      console.log(data);
     } catch (error) {
-        console.error('Error al ejecutar función:', error);
+      console.error('Error al ejecutar función:', error);
     }
     mostrarSeccion('requerimiento2');
     actualizarClaseActive('boton-requerimiento-2');
   });
 
+  // Botón para Requerimiento 3
   document.getElementById('boton-requerimiento-3').addEventListener('click', async () => {
     try {
       const respuesta = await fetch('http://127.0.0.1:5000/api/requerimientos/3', {
-          method: 'GET',
-          headers: { 'Content-Type': 'application/json' }
+        method: 'GET',
+        headers: { 'Content-Type': 'application/json' }
       });
       const data = await respuesta.json();
-      document.getElementById('image_actitudes_counts').src = `data:image/jpeg;base64,${data.image_actitudes_counts}`;
-      document.getElementById('image_computacionales_counts').src = `data:image/jpeg;base64,${data.image_computacionales_counts}`;
-      document.getElementById('image_investigación_counts').src = `data:image/jpeg;base64,${data.image_investigación_counts}`;
-      document.getElementById('image_estrategia_counts').src = `data:image/jpeg;base64,${data.image_estrategia_counts}`;
-      document.getElementById('image_habilidades_counts').src = `data:image/jpeg;base64,${data.image_habilidades_counts}`;
-      document.getElementById('image_evaluación_counts').src = `data:image/jpeg;base64,${data.image_evaluación_counts}`;
-      document.getElementById('image_herramienta_counts').src = `data:image/jpeg;base64,${data.image_herramienta_counts}`;
-      document.getElementById('image_medio_counts').src = `data:image/jpeg;base64,${data.image_medio_counts}`;
-      document.getElementById('image_escolaridad_counts').src = `data:image/jpeg;base64,${data.image_escolaridad_counts}`;
-      document.getElementById('image_psicométricas_counts').src = `data:image/jpeg;base64,${data.image_psicométricas_counts}`;
+      console.log(data);
     } catch (error) {
-        console.error('Error al ejecutar función:', error);
+      console.error('Error al ejecutar función:', error);
     }
     mostrarSeccion('requerimiento3');
     actualizarClaseActive('boton-requerimiento-3');
   });
 
+  // Botón para Requerimiento 4
   document.getElementById('boton-requerimiento-4').addEventListener('click', async () => {
     try {
       const respuesta = await fetch('http://127.0.0.1:5000/api/requerimientos/4', {
-          method: 'GET',
-          headers: { 'Content-Type': 'application/json' }
+        method: 'GET',
+        headers: { 'Content-Type': 'application/json' }
       });
       const data = await respuesta.json();
-      document.getElementById('image_wordcloud').src = `data:image/jpeg;base64,${data.image_wordcloud}`;
+      console.log(data);
     } catch (error) {
-        console.error('Error al ejecutar función:', error);
+      console.error('Error al ejecutar función:', error);
     }
     mostrarSeccion('requerimiento4');
     actualizarClaseActive('boton-requerimiento-4');
   });
 
+  // Botón para Requerimiento 5
   document.getElementById('boton-requerimiento-5').addEventListener('click', async () => {
     try {
       const respuesta = await fetch('http://127.0.0.1:5000/api/requerimientos/5', {
-          method: 'GET',
-          headers: { 'Content-Type': 'application/json' }
+        method: 'GET',
+        headers: { 'Content-Type': 'application/json' }
       });
       const data = await respuesta.json();
-      document.getElementById('image_journal_graph').src = `data:image/jpeg;base64,${data.image_journal_graph}`;
+      console.log(data);
     } catch (error) {
-        console.error('Error al ejecutar función:', error);
+      console.error('Error al ejecutar función:', error);
     }
     mostrarSeccion('requerimiento5');
     actualizarClaseActive('boton-requerimiento-5');
